a999b99efd152a1c5eeffbd2ff6a1b78f5be5adc
Lucas Lomar <lomarlucas@gmail.com>
2021-04-24 13:38:06 -0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,19 +1,31 @@
-import os
-#Tela Inicial
+import os #INPORT DE COMANDOS DO SISTEMA OPERACIONAL (WINDOWS)
 
-def telainicial():
-    print("\nBEM VINDO\n")
-    print("Escolha seu nome:\n")
-    nome=input()
-    os.system("cls")
+#AS CLASSES FICAM AQUI EM CIMA kkkkkkk
 
-def gerarpersonagem():
+#PERSONAGEM
+class Personagem:
     nivel=0
     hp=10
     mp=10
     arma=0
     armadura=0
 
+#AS FUNCOES FICAM AQUI EMBAIXO KKKKKK
+
+#FUNCAO EXIBE A TELA INICIAL DO JOGO
+def telainicial():
+    print("\nBEM VINDO\n")
+    print("Escolha seu nome:\n")
+    nome=input()
+    os.system("cls")
+
+
+#FUNCAO GERA UM NOVO PERSONAGEM (ATRIBUTOS INICIAIS)
+def gerarpersonagem():
+    Personagem.nivel=1
+    print("Nivel: %i\n" %Personagem.nivel)
+
+#FUNCAO EXIBE ELEMENTOS DO MAPA NA TELA
 def mostrarmapa():
     print("\nLOCALIZAÇÃO: CIDADE CAPITAL\n")
     print(" 1 - TABERNA")
@@ -23,15 +35,31 @@ def mostrarmapa():
     print(" 5 - CASTELO")
     print(" 6 - FLORESTA")
     print("\n")
-    prin("Tecle um numero para ir:\n")
+    print("Tecle um numero para ir:\n")
     edificio=input()
 
-def novojogo():
-    mostrarmapa()
+#EXIBE O STATUS ATUAL DO PERSONAGEM
+def verstatus():
+    #print("Nome: %s" %Personagem.nome)
+    print("Nivel: %i" %Personagem.nivel)
+    print("HP: %i" %Personagem.hp)
+    print("MP: %i" %Personagem.mp)
+    print("Arma: %i" %Personagem.arma)
+    print("Armadura: %i" %Personagem.armadura)
 
+def novojogo():
+    print("\nM - Exibir Mapa:      S - Exibir Statu:\n")
+    acao=input()
+    acao=acao.upper()
+    os.system("cls")
+
+    if acao == "M":
+        mostrarmapa()
+    if acao == "S":
+        verstatus()
 
 
 #Comandos do Jogo
 telainicial()
-#gerarpersonagem()
+gerarpersonagem()
 novojogo()
